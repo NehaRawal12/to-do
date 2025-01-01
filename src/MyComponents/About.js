769ea05acca11bd1c5d02fb08b3b215f98c8dae1
NehaRawal12 <neharawal22@navgurukul.org>
2025-01-01 12:01:24 +0530

    first changes

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an about component
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Eos repudiandae enim, exercitationem vel, quos, 
+            tenetur expedita atque doloribus vitae quo voluptates 
+            non modi repellendus voluptatum animi esse fuga incidunt distinctio?</p>
+    </div>
+  )
+}
